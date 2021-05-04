c8c2eb19e8f037181f4f40943f8629e9c97f34aa
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-04 16:13:51 +0100

    black/white etch-a-sketch done

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const container = document.getElementById("container");
 for (let i = 0; i < 256; i++) {
   container.innerHTML += '<div class="square"></div>';
 }
+
+const divs = document.querySelectorAll(".square");
+
+function changeColour(e) {
+  e.target.style.backgroundColor = "black";
+}
+
+for (let j = 0; j < divs.length; j++) {
+  divs[j].addEventListener("mouseenter", changeColour);
+}
