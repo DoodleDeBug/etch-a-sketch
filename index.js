fef4a922554f48925f1e24abb35630bd7c7c0083
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-10 16:03:29 +0100

    enable erasure and mono buttons

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const container = document.getElementById("container");
 
+const mono = document.getElementById("mono");
+const multi = document.getElementById("multi");
+const rubber = document.getElementById("rubber");
+const resize = document.getElementById("resize");
+const clear = document.getElementById("clear");
+
+//make initial grid
 for (let i = 0; i < 256; i++) {
   container.innerHTML += '<div class="square"></div>';
 }
 
 const divs = document.querySelectorAll(".square");
 
-function changeColour(e) {
-  e.target.style.backgroundColor = "black";
+function Mono() {
+  for (let j = 0; j < divs.length; j++) {
+    divs[j].addEventListener("mouseenter", function (e) {
+      e.target.style.backgroundColor = "black";
+    });
+  }
 }
 
-for (let j = 0; j < divs.length; j++) {
-  divs[j].addEventListener("mouseenter", changeColour);
+function Rubber() {
+  for (let j = 0; j < divs.length; j++) {
+    divs[j].addEventListener("mouseenter", function (e) {
+      e.target.style.backgroundColor = "white";
+    });
+  }
 }
+
+mono.addEventListener("click", Mono);
+rubber.addEventListener("click", Rubber);
