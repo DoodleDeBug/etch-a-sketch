9d94354a148b694108402acf400b1bc996cb6b34
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-11 16:52:38 +0100

    more ui work

--- index.js
+++ index.js
@@ -9,17 +9,13 @@ const clear = document.getElementById("clear");
 const resize = document.getElementById("resize");
 
 //make initial grid
+let size = 16;
+
 for (let i = 0; i < 256; i++) {
   container.innerHTML += '<div class="square"></div>';
 }
 
 const divs = document.querySelectorAll(".square");
-
-let nums = [];
-
-for (let i = 0; i < 256; i++) {
-  nums.push(i);
-}
 
 function Mono() {
   for (let j = 0; j < divs.length; j++) {
@@ -32,7 +28,7 @@ function Mono() {
 function Grey() {
   for (let j = 0; j < divs.length; j++) {
     divs[j].addEventListener("mouseenter", function (e) {
-      let random = Math.floor(Math.random() * nums.length);
+      let random = Math.floor(Math.random() * 256);
       e.target.style.backgroundColor = `rgb(${random},${random},${random})`;
     });
   }
@@ -41,13 +37,14 @@ function Grey() {
 function Multi() {
   for (let j = 0; j < divs.length; j++) {
     divs[j].addEventListener("mouseenter", function (e) {
-      let c1 = Math.floor(Math.random() * nums.length);
-      let c2 = Math.floor(Math.random() * nums.length);
-      let c3 = Math.floor(Math.random() * nums.length);
+      let c1 = Math.floor(Math.random() * 256);
+      let c2 = Math.floor(Math.random() * 256);
+      let c3 = Math.floor(Math.random() * 256);
       e.target.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
     });
   }
 }
+
 function Rubber() {
   for (let j = 0; j < divs.length; j++) {
     divs[j].addEventListener("mouseenter", function (e) {
@@ -62,6 +59,15 @@ function Clear() {
   }
 }
 
+// function Resize() {
+//  size = prompt("Choose a grid size (between 1-100)", 16);
+
+//   for (let j = 0; j < divs.length; j++) {
+//     divs[j].style.backgroundColor = "white";
+//   }
+// }
+
+Mono();
 mono.addEventListener("click", Mono);
 grey.addEventListener("click", Grey);
 multi.addEventListener("click", Multi);
