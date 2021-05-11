e50a89c50c2e871b8ff9ac6f4b43f1570f82b8e2
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-11 12:51:38 +0100

    add greyscale

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("container");
 
 const mono = document.getElementById("mono");
+const grey = document.getElementById("grey");
 const multi = document.getElementById("multi");
 const rubber = document.getElementById("rubber");
 const resize = document.getElementById("resize");
@@ -13,10 +14,25 @@ for (let i = 0; i < 256; i++) {
 
 const divs = document.querySelectorAll(".square");
 
+let nums = [];
+
+for (let i = 0; i < 256; i++) {
+  nums.push(i);
+}
+
 function Mono() {
   for (let j = 0; j < divs.length; j++) {
     divs[j].addEventListener("mouseenter", function (e) {
       e.target.style.backgroundColor = "black";
+    });
+  }
+}
+
+function Grey() {
+  for (let j = 0; j < divs.length; j++) {
+    divs[j].addEventListener("mouseenter", function (e) {
+      let random = Math.floor(Math.random() * nums.length);
+      e.target.style.backgroundColor = `rgb(${random},${random},${random})`;
     });
   }
 }
@@ -30,4 +46,5 @@ function Rubber() {
 }
 
 mono.addEventListener("click", Mono);
+grey.addEventListener("click", Grey);
 rubber.addEventListener("click", Rubber);
